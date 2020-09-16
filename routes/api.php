b1061d85de32586a8b21f67d51bab61879a2552b
Andip114 <andipurwanto912@gmail.com>
2020-09-16 15:54:11 +0700

    customer transaction

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('city', 'Ecommerce\CartController@getCity'); //ROUTE API UNTUK /CITY
+Route::get('district', 'Ecommerce\CartController@getDistrict'); //ROUTE API UNTUK /DISTRICT
